edab0ad8af5afa173bd3d7daa40328def6b2b0e9
macodroid <macko.andrej1@gmail.com>
2022-11-04 22:06:01 +0100

    run test

---
+++ run_test.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+#python main.py --config config_relu1.toml
+#python main.py --config config_relu2.toml
+#python main.py --config config_sigmoid1.toml
+#python main.py --config config_sigmoid2.toml
+#python main.py --config config_leaky1.toml
+#python main.py --config config_leaky2.toml
+#python main.py --config config_leaky3.toml
+#python main.py --config config_silu.toml
+#python main.py --config config_gelu.toml
+
+python main.py --config config_adam1.toml
+python main.py --config config_adam2.toml
+python main.py --config config_adam3.toml
+python main.py --config config_adam4.toml
+
+python main.py --config config_sgd1.toml
+python main.py --config config_sgd2.toml
+python main.py --config config_sgd3.toml
+python main.py --config config_sgd4.toml
+
+python main.py --config config_rmsprop1.toml
+python main.py --config config_rmsprop2.toml
+python main.py --config config_rmsprop3.toml
+python main.py --config config_rmsprop4.toml
